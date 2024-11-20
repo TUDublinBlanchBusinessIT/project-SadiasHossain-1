6c5df712dcdea1dfa1e8d3bfabc22df8fc9ce5ca
B00151316 Sadia Hossain <b00151316@mytudublin.ie>
2024-11-20 18:13:17 +0000

    opening screen

---
+++ screens/OpeningScreen.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, Button, TouchableOpacity } from 'react-native';
+
+const OpeningScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/logo.jpg')} style={styles.logo} />
+      <TouchableOpacity style={styles.button} onPress={() => alert('Navigate to Login')}>
+        <Text style={styles.buttonText}>Log In</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => alert('Navigate to Sign In')}>
+        <Text style={styles.buttonText}>Sign In</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  logo: {
+    width: 250,
+    height: undefined,
+    aspectRatio: 1,
+    resizeMode: 'contain',
+    marginBottom: 20
+  },
+  button: {
+    backgroundColor: '#800020',
+    padding: 15,
+    borderRadius: 10,
+    width: '60%', // Reduce width to 60% of the screen
+    maxWidth: 300,
+    marginVertical: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: '600',
+  },
+});
+
+export default OpeningScreen;
